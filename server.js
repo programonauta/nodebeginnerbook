29816467195ef6bae6630f8f30e6365a9feb70a1
Ricardo Brandao <rbrandao.br@gmail.com>
2014-08-29 14:22:23 -0300

    Second version. With console messages

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-/* 
-	First version of simple HTTP server
+/*
+	Second version of simple HTTP server
+	With console messages
 */
 
 var http = require("http");
 
-// The callback function is defined as a parameter of createServer method.
+/*
+	The callback function is defined as a
+	parameter of createServer method.
+*/
 
 /* 	To run the server:
 	$>node server.js
@@ -12,10 +16,20 @@ var http = require("http");
 	You'll see no output on the terminal
 	On the browser: http://localhost:8888
 	You'll see a Hello World! as a response from your server
+
+	In this version, you'll see a message on the terminal saying
+	the server had started.
+	This is to show that the call back function will be called only
+	when a request arrvied on the server.
+
 */
 
-http.createServer(function(request, response) {
+function onRequest(request, response) {
+	console.log("Request received.");
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("Hello World!");
 	response.end();
-}).listen(8888);
+}
+
+http.createServer(onRequest).listen(8888);
+console.log("Server has started.");
